ce8fb4757d1d12d3bbe922e5fc250bf60a00900f
Robert <robert.j.rantanen@helsinki.fi>
2019-08-21 15:19:52 +0300

    puhelin backend 3

--- index.js
+++ index.js
@@ -34,6 +34,17 @@ app.get('/info', (req, res) => {
   + "<h3>" + new Date() + "</h3")
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 
 const PORT = 3001
 app.listen(PORT)
