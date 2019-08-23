7e7d59b517eff228b92c1cc1b89d8f3183606c39
Robert <robert.j.rantanen@helsinki.fi>
2019-08-23 20:18:36 +0300

    3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0-zdkyg.mongodb.net/puhelinluettelo-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+
+if ( process.argv.length===3 ) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+      })
+  } else {
+    person.save().then(response => {
+        console.log('added ' + person.name + " number " + person.number + " to phonebook");
+        mongoose.connection.close();
+      })
+  }
+  
