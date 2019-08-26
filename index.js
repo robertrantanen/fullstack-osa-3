d4f73dc81ab746f10a3a6799b3e06025271aac00
Robert <robert.j.rantanen@helsinki.fi>
2019-08-26 15:18:21 +0300

    3.16 ja 3.17

--- index.js
+++ index.js
@@ -41,11 +41,12 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 
@@ -58,6 +59,16 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
+  /*
+  const personNames = persons.map(p => p.name)
+
+  if (personNames.includes(body.name)) {
+    return res.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  */
+
   const person = new Person({
     name: body.name,
     number: body.number
@@ -68,16 +79,42 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
 
-/*
-  const personNames = persons.map(p => p.name)
-
-  if (personNames.includes(body.name)) {
-    return res.status(400).json({ 
-      error: 'name must be unique' 
-    })
+  const person = {
+    name: body.name,
+    number: body.number,
   }
-  */
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+
+
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 
