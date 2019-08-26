98cde1a58e7d613a35fd744558c17c2c311abf3d
Robert <robert.j.rantanen@helsinki.fi>
2019-08-26 15:44:23 +0300

    3.18

--- index.js
+++ index.js
@@ -26,19 +26,24 @@ app.get('/api/persons', (req, res) => {
 });
 
 app.get('/info', (req, res) => {
-  res.send("<h3>Phonebook has info for " + persons.length + " people</h3>"
-  + "<h3>" + new Date() + "</h3")
+
+  Person.count().then(count => {
+    res.send("<h3>Phonebook has info for " + count + " people</h3>"
+    + "<h3>" + new Date() + "</h3")
+  });
+  
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
